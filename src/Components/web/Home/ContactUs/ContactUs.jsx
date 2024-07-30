a113182a7eb64fa1032ebbacdf1716a7651bba4b
Hadeel-Abdeljalil <h.02.adeel@gmail.com>
2024-07-30 16:49:21 +0300

    update location

--- src/Components/web/Home/ContactUs/ContactUs.jsx
+++ src/Components/web/Home/ContactUs/ContactUs.jsx
@@ -61,8 +61,15 @@ export default function ContactUs() {
               </div>
             </div>
             <div className="map-iframe col-lg-5">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13547.12180452607!2d35.211417255678164!3d31.912562084585705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151cd55d8aad2f91%3A0x94071f835d2893a4!2sAl-Bireh!5e0!3m2!1sen!2s!4v1721906409276!5m2!1sen!2s" width={600} height={450} style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
-
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3386.3920738116994!2d35.2144167!3d31.923111100000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDU1JzIzLjIiTiAzNcKwMTInNTEuOSJF!5e0!3m2!1sen!2s!4v1722347279090!5m2!1sen!2s"
+                width={600}
+                height={450}
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
             </div>
           </div>
         </div>
